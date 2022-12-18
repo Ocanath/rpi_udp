f138aa1ca573b5fa52ea19c68cb9ce8b03ecca2a
ocanath <ocanath@gmail.com>
2022-12-18 05:30:26 +0000

    non blocking works

--- src/main.c
+++ src/main.c
@@ -11,17 +11,46 @@
 #include <net/if.h>
 #include <sys/ioctl.h>
 
+
+typedef union u32_fmt_t
+{
+	uint32_t u32;
+	int32_t i32;
+	float f32;
+	int16_t i16[sizeof(uint32_t) / sizeof(int16_t)];
+	uint16_t ui16[sizeof(uint32_t) / sizeof(uint16_t)];
+	int8_t i8[sizeof(uint32_t) / sizeof(int8_t)];
+	uint8_t ui8[sizeof(uint32_t) / sizeof(uint8_t)];
+}u32_fmt_t;
+
+
+/*Setup server parameters*/
 #define PORT 9145
 #define MAX_UDP_RX_SIZE 1024
 uint8_t rx_buf[MAX_UDP_RX_SIZE];
+u32_fmt_t * fmt_buf = (u32_fmt_t*)(&rx_buf[0]);
+
+
+/*
+Generic hex checksum calculation.
+TODO: use this in the psyonic API
+*/
+uint32_t get_checksum32(uint32_t* arr, int size)
+{
+	int32_t checksum = 0;
+	for (int i = 0; i < size; i++)
+		checksum += (int32_t)arr[i];
+	return -checksum;
+}
+
 
 void main(void)
 {
 	int sockfd;	
 	struct sockaddr_in servaddr, cliaddr; 
         
-	// Creating socket file descriptor 
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) 
+	// Creating socket file descriptor . include nonblocking flag here for... nonblocking functionality
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM | SOCK_NONBLOCK, 0)) < 0 ) 
 	{ 
 		perror("socket creation failed"); 
 		exit(EXIT_FAILURE); 
@@ -52,13 +81,40 @@ void main(void)
         exit(EXIT_FAILURE); 
     }
 	printf("Bind success\r\n");
-	
+		
+	int cli_len = sizeof(cliaddr);
 	while(1)
 	{
-		int len = sizeof(cliaddr);
-	    int n = recvfrom(sockfd, (char *)rx_buf, MAX_UDP_RX_SIZE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len); 
-		rx_buf[n] = '\0';
-		memset(disp_buf, 0, sizeof(disp_buf));
-		printf("%s sent %s\r\n", inet_ntoa(cliaddr.sin_addr), rx_buf);
+		
+	    int n = recvfrom(sockfd, (char *)rx_buf, MAX_UDP_RX_SIZE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &cli_len); 
+		if(n == -1)
+		{
+			//waiting
+		}
+		else
+		{
+			rx_buf[n] = '\0';
+			uint8_t chk_match=0;
+			if(n % sizeof(u32_fmt_t) == 0)	//32bit checksum only applies if we have a multiple of 4 length data input
+			{
+				int u32_len = n/sizeof(u32_fmt_t);
+				if(get_checksum32((uint32_t*)rx_buf, u32_len-1) == fmt_buf[u32_len - 1].u32)
+				{
+					chk_match = 1;
+					
+					//print formatted datas
+					printf("%s sent: ", inet_ntoa(cliaddr.sin_addr));
+					for(int i = 0; i < u32_len-2; i++)
+					{
+						printf("%0.4f, ", fmt_buf[i].f32);
+					}
+					printf("%0.4f\r\n", fmt_buf[u32_len-2].f32);
+				}
+			}
+			else if(chk_match == 0)
+			{
+				printf("%s sent %s\r\n", inet_ntoa(cliaddr.sin_addr), rx_buf);
+			}
+		}
 	}
 }
